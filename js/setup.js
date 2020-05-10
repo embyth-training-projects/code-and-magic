a9080ca2a4a82d373b06e686461255378ac8ea3d
Rostyslav Miniukov <thresh1337@gmail.com>
2020-05-10 19:31:52 +0300

    Adding validity handler for input character name

--- js/setup.js
+++ js/setup.js
@@ -170,3 +170,28 @@ setupNameInput.addEventListener('focus', function () {
 setupNameInput.addEventListener('blur', function () {
   document.addEventListener('keydown', popupEscPressHandler);
 });
+
+// Кастомные сообщение об ошибках валидации
+var validityMessages = {
+  TOO_SHORT: 'Имя персонажа должно состоять не менее чем из 2-х символов',
+  TOO_LONG: 'Имя персонажа не должно превышать 25-ти символов',
+  VALUE_MISSING: 'Дайте имя вашему персонажу'
+};
+
+setupNameInput.addEventListener('invalid', function () {
+  if (setupNameInput.validity.tooShort) {
+    setupNameInput.setCustomValidity(validityMessages.TOO_SHORT);
+  } else if (setupNameInput.validity.tooLong) {
+    setupNameInput.setCustomValidity(validityMessages.TOO_LONG);
+  } else if (setupNameInput.validity.valueMissing) {
+    setupNameInput.setCustomValidity(validityMessages.VALUE_MISSING);
+  } else {
+    setupNameInput.setCustomValidity('');
+  }
+});
+
+setupNameInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+
+  target.setCustomValidity('');
+});
